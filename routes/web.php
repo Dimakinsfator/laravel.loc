455199b6b1748dd60a50d93b5480ae12203b6ee9
kinsfator-de <dkins@yandex.ru>
2024-10-15 19:04:42 +0700

    изучили маршрут

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/test1', function () {
+   return'Hello World';
+});
+
+Route::redirect('/test2','test1', 302);
+Route::permanentRedirect('/test3', 'test1');
+
+Route::view('/test4', 'welcome'[]);
